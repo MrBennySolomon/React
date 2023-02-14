dce2a16d652beb85f2d8a57479c5c3e0d914154f
Benny Solomon <mr.benny.solomon@gmail.com>
2023-02-14 12:01:45 +0200

    6.3_increment_and_decrement

--- REACT-TEMPLATE/my-app/src/App.js
+++ REACT-TEMPLATE/my-app/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import IncrementAndDecrement from './IncrementAndDecrement';
 function App() {
   return (
     <div className="App">
-      
+      <IncrementAndDecrement counter='0' zero='zero' positive='positive' negative='negative'/>
     </div>
   );
 }
