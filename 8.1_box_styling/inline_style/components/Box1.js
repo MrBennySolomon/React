86cf9801ecc1754eebfdcbcd7d66711c16e14a57
Benny Solomon <mr.benny.solomon@gmail.com>
2023-02-15 14:34:13 +0200

    8.1_box_styling

---
+++ 8.1_box_styling/inline_style/components/Box1.js
@@ -0,0 +1,19 @@
+import Box2 from "./Box2"
+
+const Box1 = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+      backgroundColor: 'lightseagreen',
+      width: '50vw',
+      height: '50vh'
+    }}>
+        <Box2/>
+    </div>
+  )
+}
+
+export default Box1
