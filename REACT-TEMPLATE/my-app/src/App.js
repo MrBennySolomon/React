a535de7d19d0d54df949c4b762921f7cf0e4245a
בני סולומון <mr.benny.solomon@gmail.com>
2023-02-20 11:29:24 +0200

    13.3_changing_box

--- REACT-TEMPLATE/my-app/src/App.js
+++ REACT-TEMPLATE/my-app/src/App.js
@@ -4,40 +4,17 @@ import { useEffect, useRef } from 'react';
 const App = () => {
 
   const refDiv1 = useRef(null);
-  const refDiv2 = useRef(null);
-  const refDiv3 = useRef(null);
+
 
   useEffect(() => {
-    let timerId1;
-    let timerId2;
-
-    timerId1 = setTimeout(() => {
-      
-      refDiv1.current.classList.add('box1');
-      refDiv2.current.classList.add('box2');
-      refDiv3.current.classList.add('box3');
-
-    }, 1000);
-    timerId2 = setTimeout(() => {
-      
-      refDiv1.current.classList.remove('box1');
-      refDiv2.current.classList.remove('box2');
-      refDiv3.current.classList.remove('box3');
-      
-    }, 4000);
-
-    return () => {
-      timerId1 && clearTimeout(timerId1);
-      timerId2 && clearTimeout(timerId2);
-    }
+    refDiv1.current.classList.add('box1');
   },[]);
 
 
   return (
     <div className='App.css'>
       <div ref={refDiv1}></div>
-      <div ref={refDiv2}></div>
-      <div ref={refDiv3}></div>
+
     </div>
   );
 };
