7ee0dd26dd7948cd88b359b464477493b6095c82
בני סולומון <mr.benny.solomon@gmail.com>
2023-02-23 15:18:33 +0200

    pic

--- REACT-TEMPLATE/my-app/src/App.js
+++ REACT-TEMPLATE/my-app/src/App.js
@@ -1,30 +1,12 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home';
-import ProductsPage from './pages/Products';
-import Root from './pages/Root'
-import data from './store';
-import Product from './pages/Product'
-import Error from './pages/Error';
+import "./App.css";
+import AppDisplay from "./components/pages/AppDisplay";
 
-
-
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <Root/>,
-    errorElement: <Error/>,
-  children: [
-    {path: '/'          , element: <HomePage/>},
-    {path: '/products/'  , element: <ProductsPage/>},
-    {path: '/products/:id', element: <Product data={data}/>}
-  ],
-  },
-]);
-
-const App = () => {
+function App() {
   return (
-        <RouterProvider router={router} />
-  )
+    <div>
+      <AppDisplay />
+    </div>
+  );
 }
+
 export default App;
---
+++ REACT-TEMPLATE/my-app/src/api/contacts.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://61c323689cfb8f0017a3e998.mockapi.io/Shoes",
+});
