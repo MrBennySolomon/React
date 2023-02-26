d81bf14f261fbb3dac901c48cce8de1fd7629c18
בני סולומון <mr.benny.solomon@gmail.com>
2023-02-26 12:41:29 +0200

    18.2_todo_reducer

--- 17.1_todo_ with_localStorage/App.js
+++ 17.1_todo_ with_localStorage/App.js
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
 import "./styles/style.css";
 
+const todoReducer = (state, action) => {
+  if (action.type === 'FILL_TODO_ARR') {
+    return { todos: [...action.data], newTodoText: state.newTodoText };
+  }else if (action.type === 'NEW_TODO_TEXT') {
+    return { todos: state.todos, newTodoText: action.newTodoText };
+  }else {
+    return { todos: [{ id: 0, text: "" }], newTodoText: "" };
+  }
+  
+};
+
 function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [newTodoText, setNewTodoText] = useState("");
+
+  const [todoState, dispatchTodo] = useReducer(todoReducer, {
+    value: [{ id: 0, text: "" }],
+    newTodoText: ""
+  });
 
   useEffect(() => {
     const data = localStorage.getItem("todos");
     if (data) {
-      setTodos(JSON.parse(data));
+      dispatchTodo({type: 'FILL_TODO_ARR', data:data})
     }
   }, []);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+  }, [todoState.todos.length]);
 
   const addTodo = () => {
-    setTodos((todos) => [...todos, { id: Date.now(), text: newTodoText }]);
-    setNewTodoText("");
+    dispatchTodo({type: 'FILL_TODO_ARR', data:[...todos, { id: Date.now(), text: newTodoText }], newTodoText: ''});
   };
 
   const handleNewTodoTextChange = (event) => {
-    setNewTodoText(event.target.value);
+    dispatchTodo({ type: 'NEW_TODO_TEXT', newTodoText: event.target.value});
   };
 
   const handleDeleteTodo = (id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    setTodos((todos) => {todos.filter((todo) => todo.id !== id)});
   };
 
   return (
@@ -36,16 +49,20 @@ function TodoList() {
       <div className="add-todo">
         <input
           type="text"
-          value={newTodoText}
+          value={todoState.newTodoText}
           onChange={handleNewTodoTextChange}
           placeholder="Enter new todo"
         />
         <button onClick={addTodo}>Add Todo</button>
       </div>
       <ul>
-        {todos.map((todo) => (
+        {todoState.todos.map((todo) => (
           <li key={todo.id}>
-            <label><input type='checkbox'/>{todo.text}</label><button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <label>
+              <input type="checkbox" />
+              {todo.text}
+            </label>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
