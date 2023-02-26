a41913360fa40f51f239b5197e2ba4b94b4e76d1
בני סולומון <mr.benny.solomon@gmail.com>
2023-02-26 12:40:01 +0200

    18.2_todo_reducer

--- 18.2_todo_reducer/App.js
+++ 18.2_todo_reducer/App.js
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
 import "./styles/style.css";
 
+const reducer = (state, target) => {
+  if (target.type === 'SET_TODO_ARR') {
+    return {todo:[...target.data], newTodoText: state.newTodoText};
+  }else if ('SET_TODO_TEXT') {
+    return {todo: state.todo, newTodoText: target.newTodoText};
+  }else {
+    return {todo:[], newTodoText: ''};
+  }
+}
+
 function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [newTodoText, setNewTodoText] = useState("");
+  // const [todos, setTodos] = useState([]);
+  // const [newTodoText, setNewTodoText] = useState("");
+
+  const [todoState, dispatcher] = useReducer(reducer, {todo:[], newTodoText: ''});
 
   useEffect(() => {
-    const data = localStorage.getItem("todos");
+    const data = localStorage.getItem("todo");
     if (data) {
-      setTodos(JSON.parse(data));
+      //setTodos(JSON.parse(data));
+      dispatcher({type: 'SET_TODO_ARR', data: JSON.parse(data)});
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    localStorage.setItem("todo", JSON.stringify(todoState.todo));
+  }, [todoState.todo]);
 
   const addTodo = () => {
-    setTodos((todos) => [...todos, { id: Date.now(), text: newTodoText }]);
-    setNewTodoText("");
+    //setTodos((todos) => [...todos, { id: Date.now(), text: newTodoText }]);
+    //setNewTodoText("");
+    dispatcher({type: 'SET_TODO_ARR', data: [...todoState.todo, { id: Date.now(), text: todoState.newTodoText }]})
+    dispatcher({type: 'SET_TODO_TEXT', data: ''})
   };
 
   const handleNewTodoTextChange = (event) => {
-    setNewTodoText(event.target.value);
+    //setNewTodoText(event.target.value);
+    dispatcher({type: 'SET_TODO_TEXT', newTodoText: event.target.value});
   };
 
   const handleDeleteTodo = (id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    //setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatcher({type: 'SET_TODO_ARR', data: todoState.todo.filter((todo) => todo.id !== id)})
   };
 
   return (
@@ -36,14 +53,14 @@ function TodoList() {
       <div className="add-todo">
         <input
           type="text"
-          value={newTodoText}
+          value={todoState.newTodoText}
           onChange={handleNewTodoTextChange}
           placeholder="Enter new todo"
         />
         <button onClick={addTodo}>Add Todo</button>
       </div>
       <ul>
-        {todos.map((todo) => (
+        {todoState.todo.map((todo) => (
           <li key={todo.id} onClick={() => handleDeleteTodo(todo.id)}>
             {todo.text}
           </li>
