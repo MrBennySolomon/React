d0e39f156175ec07a471f2e42407cdea5246c488
Benny Solomon <mr.benny.solomon@gmail.com>
2023-03-01 11:52:08 +0200

    24.1_useCounter

--- 24.1_useCounter/App.js
+++ 24.1_useCounter/App.js
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react';
-import useCounter from './hooks/useCounter';
-const App = () => {
-  const counter = useCounter(5);
-  useEffect(() => {
-    
+import useCounter from './hooks/useCounter'
 
-  
-  console.log('counter.addOne()', counter.addOne());
-  }, []);
-  
-  // console.log(counter.addOne());
-  // console.log(counter.addOne());
-  // console.log(counter.addOne());
+const App = () => {  
+  const {
+    counter,
+    addOne,
+    removeOne,
+    double,
+    divide
+  } = useCounter(5);
 
-  return (
-    <div>App</div>
+  const oneAdded= addOne()
+  const oneRemoved= removeOne()
+  const doubled= double()
+
+  return(
+    <>
+      <h2>{counter}</h2>
+      <h2>{oneRemoved}</h2>
+      <h2>{removeOne}</h2>
+      <h2>{double}</h2>
+      <h2>{divide}</h2>
+    </>
   )
-}
-
+  
+};
 export default App;
-
-
--- 24.1_useCounter/hooks/useCounter.js
+++ 24.1_useCounter/hooks/useCounter.js
@@ -1,49 +1,32 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const useCounter = (num) => {
   const [counter, setCounter] = useState(num);
 
-  
-
   const addOne = () => {
-    setCounter((prev) => prev + 1); 
-    console.log(counter)
-    return counter;
+    setCounter((prevNum)=> prevNum+1)
+    return counter
   }
   const removeOne = () => {
-    setCounter((prev) => prev - 1); 
-    return counter;
+    setCounter((prevNum)=> prevNum-1)
+    return counter
   }
   const double = () => {
-    setCounter((prev) => prev * 2); 
-    return counter;
+    setCounter((prevNum)=> prevNum*2)
+    return counter
   }
   const divide = () => {
-    setCounter((prev) => prev / 2); 
-    return counter;
+    setCounter((prevNum)=> prevNum/2)
+    return counter
   }
 
   return {
-    counter: counter,
-    addOne: addOne,
-    removeOne: removeOne,
-    double: double,
-    divide: divide
-  };
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    counter,
+    addOne,
+    removeOne,
+    double,
+    divide
+  }
+};
 
 export default useCounter;
